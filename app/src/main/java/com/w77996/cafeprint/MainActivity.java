e3152ccec30cd17b1bc7a0fafc1ee35249fc8702
w77996 <912513404@qq.com>
2017-12-27 23:44:29 +0800

    view的绘制及img位移

--- app/src/main/java/com/w77996/cafeprint/MainActivity.java
+++ app/src/main/java/com/w77996/cafeprint/MainActivity.java
@@ -1,13 +1,43 @@
 package com.w77996.cafeprint;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.w77996.cafeprint.utils.MyMatrixImg;
+import com.w77996.cafeprint.view.CircleRelativeLayout;
+import com.w77996.cafeprint.view.CustomViews;
 
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    Button mPhoto;
+    //CustomViews mCustomViews;
+    MyMatrixImg mMyMatrixImg;
+    CircleRelativeLayout mview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mview = new CircleRelativeLayout(getApplicationContext());
+        mMyMatrixImg = new MyMatrixImg(getApplicationContext());
+        mview.addView(mMyMatrixImg);
+        mPhoto = (Button)findViewById(R.id.btn_photo);
+       // mCustomViews = (CustomViews) findViewById(R.id.cv);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_photo:
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/w77996/cafeprint/view/CustomViews.java
@@ -0,0 +1,82 @@
+package com.w77996.cafeprint.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+/**
+ * Created by w77996
+ * on 2017/12/27.
+ * Github:https://github.com/w77996
+ */
+public class CustomViews extends ViewGroup{
+
+    private Paint mPaint;
+    private Context mContext;
+    private int screenWidth;
+    private int screenHeight;
+    private int radial = 50;
+
+    public CustomViews(Context context) {
+        super(context, null);
+        init();
+    }
+
+    public CustomViews(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        init();
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+
+    /**
+     * 由于onDraw()方法会不停的绘制 所以需要定义一个初始化画笔方法 避免导致不停创建造成内存消耗过大
+     */
+    private void init() {
+        mPaint = new Paint();
+        // 设置画笔为抗锯齿
+        mPaint.setAntiAlias(true);
+        // 设置颜色为红色
+        mPaint.setColor(Color.RED);
+        /**
+         * 画笔样式分三种： 1.Paint.Style.STROKE：描边 2.Paint.Style.FILL_AND_STROKE：描边并填充
+         * 3.Paint.Style.FILL：填充
+         */
+        mPaint.setStyle(Paint.Style.STROKE);
+        /**
+         * 设置描边的粗细，单位：像素px 注意：当setStrokeWidth(0)的时候描边宽度并不为0而是只占一个像素
+         */
+        mPaint.setStrokeWidth(20);
+        /**
+         * 获取屏幕的宽高
+         */
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        screenWidth = display.getWidth();
+        screenHeight = display.getHeight();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        /**
+         * 绘制圆环drawCircle的前两个参数表示圆心的XY坐标， 这里我们用到了一个工具类获取屏幕尺寸以便将其圆心设置在屏幕中心位置，
+         * 第三个参数是圆的半径，第四个参数则为我们的画笔
+         *
+         */
+        canvas.drawCircle(screenWidth , screenWidth, radial, mPaint);
+
+    }
+
+
+}
